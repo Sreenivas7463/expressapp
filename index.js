f1a12173ab4fcc70f41b5008d9b25f503a27fe0b
T. Sreenivasulu <59993636+Sreenivas7463@users.noreply.github.com>
2026-05-27 12:34:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,4 +7,7 @@ app.use(express.json({ extended: false }));
 app.use("/api/product", product);
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
+});
